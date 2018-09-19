8b95568e4f859be303efd6bf93caa43045a78987
tuhongwu23 <1414703945@qq.com>
2018-09-19 17:43:07 +0800

    第一次提交

---
+++ RemoteDestop/RemoteDestop/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace RemoteDestop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnCLose = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnFull = new System.Windows.Forms.Button();
+            this.rdp = new AxMSTSCLib.AxMsTscAxNotSafeForScripting();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rdp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(735, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCLose
+            // 
+            this.btnCLose.Location = new System.Drawing.Point(906, 25);
+            this.btnCLose.Name = "btnCLose";
+            this.btnCLose.Size = new System.Drawing.Size(75, 23);
+            this.btnCLose.TabIndex = 4;
+            this.btnCLose.Text = "关闭";
+            this.btnCLose.UseVisualStyleBackColor = true;
+            this.btnCLose.Click += new System.EventHandler(this.btnCLose_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnFull);
+            this.panel1.Controls.Add(this.txtname);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtPwd);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtUser);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnCLose);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1080, 68);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "用户名：";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(303, 25);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 21);
+            this.txtUser.TabIndex = 1;
+            this.txtUser.Text = "administrator";
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(460, 25);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.PasswordChar = '*';
+            this.txtPwd.Size = new System.Drawing.Size(123, 21);
+            this.txtPwd.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(419, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "密码：";
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(84, 25);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(141, 21);
+            this.txtname.TabIndex = 0;
+            this.txtname.Text = "192.168.9.141";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "计算机：";
+            // 
+            // btnFull
+            // 
+            this.btnFull.Location = new System.Drawing.Point(816, 25);
+            this.btnFull.Name = "btnFull";
+            this.btnFull.Size = new System.Drawing.Size(75, 23);
+            this.btnFull.TabIndex = 8;
+            this.btnFull.Text = "关闭连接";
+            this.btnFull.UseVisualStyleBackColor = true;
+            this.btnFull.Click += new System.EventHandler(this.btnFull_Click);
+            // 
+            // rdp
+            // 
+            this.rdp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rdp.Enabled = true;
+            this.rdp.Location = new System.Drawing.Point(0, 0);
+            this.rdp.Name = "rdp";
+            this.rdp.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("rdp.OcxState")));
+            this.rdp.Size = new System.Drawing.Size(1080, 762);
+            this.rdp.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 762);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.rdp);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "远程工具连接 V1.1.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rdp)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxMSTSCLib.AxMsTscAxNotSafeForScripting rdp;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCLose;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnFull;
+    }
+}
+
---
+++ RemoteDestop/RemoteDestop/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using MSTSCLib;
+
+namespace RemoteDestop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string jsj = txtname.Text.Trim();
+            string user = txtUser.Text.Trim();
+            string pwd = txtPwd.Text.Trim();
+
+            if (string.IsNullOrEmpty(jsj) || string.IsNullOrEmpty(user) || string.IsNullOrEmpty(pwd))
+            {
+                MessageBox.Show("所填项目不能为空！");
+                return;
+            }
+            //Thread start = new Thread(() =>
+            //{
+            try
+            {
+                rdp.Server = jsj;// "192.168.9.141";
+                rdp.UserName = user;// "Matt";// "administrator";
+                IMsTscNonScriptable secured = (IMsTscNonScriptable)rdp.GetOcx();
+                secured.ClearTextPassword = pwd;// "zeda";
+                rdp.DesktopHeight = 630;
+                rdp.DesktopWidth = panel1.Width;// 950;
+
+                rdp.Connect();
+                //MessageBox.Show("成功");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("无法连接");
+            }
+            //});
+            //start.IsBackground = true;
+            //start.Start();
+            //MessageBox.Show("成功");
+        }
+
+        private void btnCLose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnFull_Click(object sender, EventArgs e)
+        {
+            //this.rdp.FullScreen = !this.rdp.FullScreen;
+            //this.rdp.FullScreenTitle = "aa";
+            if (rdp.Connected.ToString() == "1")
+                rdp.Disconnect();
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("确认退出吗?", "操作提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (result == DialogResult.OK)
+            {
+                Dispose();
+                Application.Exit();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
